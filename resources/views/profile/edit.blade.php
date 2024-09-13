24afb7d91d3ddd9766a90b7f7ad22da7e11b5723
frankiller95 <frank95299@gmail.com>
2024-09-13 18:14:57 -0500

    ajustes-profile
    quitando el delete account

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -26,13 +26,13 @@
             </div>
 
             <!-- Mostrar solo si el rol es Admin (rol_id == 1) -->
-            @if (Auth::user()->rol_id == 1)
+            {{-- @if (Auth::user()->rol_id == 1)
                 <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                     <div class="max-w-xl">
                         @include('profile.partials.delete-user-form')
                     </div>
                 </div>
-            @endif
+            @endif --}}
         </div>
     </div>
 @stop
